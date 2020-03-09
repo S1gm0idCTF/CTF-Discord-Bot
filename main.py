ab998b047cfd47be3af8ed9969269b3a425f4e3e
Zach Battleman <blackcoffee@Zachs-MacBook-Pro.local>
2020-03-09 12:56:52 -0400

    added sanity check command

--- main.py
+++ main.py
@@ -21,14 +21,13 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-	print(message)
 	print("Message from {0.author}: {0.content} in channel: {0.channel}".format(message))
 
 	await bot.process_commands(message)
 
 @bot.command()
 async def test(ctx):
-	print("hi")
+	await ctx.send("Commands are working!")
 
 
 bot.run(TOKEN)
