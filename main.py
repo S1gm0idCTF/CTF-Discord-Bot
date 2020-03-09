83162ca2797963414ad745fe3a09bf83e49b9f15
Zach Battleman <blackcoffee@Zachs-MacBook-Pro.local>
2020-03-09 12:47:23 -0400

    added test command

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import discord
+from discord.ext import commands
+
 
 
 f = open("keys.txt", "r")
 TOKEN = f.readline().strip()
-serverID= int(f.readline().strip())
+serverID = int(f.readline().strip())
 
 print(TOKEN)
 print(serverID)
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+bot = commands.Bot(command_prefix='!')
 
-client = MyClient()
-client.run(TOKEN)
+@bot.event
+async def on_ready():
+	print("Logged on as")
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
+
+@bot.event
+async def on_message(message):
+	print(message)
+	print("Message from {0.author}: {0.content} in channel: {0.channel}".format(message))
+
+	await bot.process_commands(message)
+
+@bot.command()
+async def test(ctx):
+	print("hi")
+
+
+bot.run(TOKEN)
