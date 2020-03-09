9677c0cb98d96ff400340934c68f58f5ab1252c1
Zach Battleman <blackcoffee@Zachs-MacBook-Pro.local>
2020-03-09 12:14:59 -0400

    first real bot commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import discord
+
+
+f = open("keys.txt", "r")
+TOKEN = f.readline().strip()
+serverID= int(f.readline().strip())
+
+print(TOKEN)
+print(serverID)
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(TOKEN)
